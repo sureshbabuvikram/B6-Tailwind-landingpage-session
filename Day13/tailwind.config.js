53aa0656e51fd65e6aa31f0000841f9083148e7c
sureshbabuvikram <sureshbabuvikram@gmail.com>
2024-09-11 20:44:40 +0530

    first commit

---
+++ Day13/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize:{
+        "cxl":"0.98rem"
+      }
+    },
+  },
+  plugins: [],
+}
+
